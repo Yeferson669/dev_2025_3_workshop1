ea3608ee082f8b9182119a2c1289ea0306a95c06
Yeferson669 <Ydguaca49@ucatolica.edu.co>
2025-08-27 23:30:37 -0500

    3 Geometric

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -40,7 +40,8 @@ class Geometria:
         Returns:
             float: Área del círculo
         """
-        pass
+        pi = 3.1416
+        return pi * radio * radio 
     
     def perimetro_circulo(self, radio):
         """
