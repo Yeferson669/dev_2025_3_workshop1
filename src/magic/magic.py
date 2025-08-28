c7a011b91337ab739f5239036d99e61f15773806
Yeferson669 <Ydguaca49@ucatolica.edu.co>
2025-08-28 00:09:25 -0500

    8 Magic

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -152,7 +152,9 @@ class Magic:
         Returns:
             int: El máximo común divisor de a y b
         """
-        pass
+        while b != 0:
+            a, b = b, a % b
+        return abs(a)
     
     def mcm(self, a, b):
         """
