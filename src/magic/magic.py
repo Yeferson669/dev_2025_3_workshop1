3720cbf4e186816c72b5e3ee2daf0704d37bf150
Yeferson669 <Ydguaca49@ucatolica.edu.co>
2025-08-28 22:45:12 -0500

    Magic correccion 2

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -34,15 +34,15 @@ class Magic:
         Returns:
             list: Lista con los primeros n números de Fibonacci
         """
-        if n == 0:
-            return 0
+        if n <= 0:
+            return []
         elif n == 1:
-            return 1
+            return [0]
         
-        a, b = 0, 1
-        for _ in range(2, n + 1):
-            a, b = b, a + b
-        return b
+        fib = [0, 1]
+        while len(fib) < n:
+            fib.append(fib[-1] + fib[-2])
+        return fib
     
     def es_primo(self, n):
         """
